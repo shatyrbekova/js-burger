333b20f7a4bb467a7a8d978271376cf54082b4e4
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-07 16:19:37 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var createPrice= document.getElementsByClassName('create-prices-btn')[0];
+var totalPrice = document.getElementById('total-price');
+
+createPrice.addEventListener('click', function(){
+
+var initialPrice = 50;
+var  priceCheck  = document.getElementsByClassName('sum-check');
+
+for (var y=0; y <priceCheck.length; y++){
+
+    if (priceCheck[y].checked) {
+        initialPrice +=parseInt (priceCheck[y].value)
+    }
+}
+     totalPrice.innerHTML = 'Your Price' + "         " + '$' +  initialPrice;
+})
