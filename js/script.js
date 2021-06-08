6f08e7dfb737e4057b50cb7ea91c8ab4b1e19811
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-08 12:57:18 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-//var nameYourBurger = confirm('Would you like to create Your Burger?')
+var nameYourBurger = confirm('Would you like to create Your Burger?')
+var nameBurgerPrompt=prompt('Pleaese, Name Your Burger.');
 var createPrice= document.getElementsByClassName('create-prices-btn')[0];
 var totalPrice = document.getElementById('total-price');
+var nameBurger = document.getElementById('name-your-burger');
 
+nameBurger.innerHTML=nameBurgerPrompt;
 
 var disCoupon = document.getElementById('dis-coupon');
 createPrice.addEventListener('click', function(){
@@ -9,6 +12,7 @@ createPrice.addEventListener('click', function(){
 
 var initialPrice = 50;
 var  priceCheck  = document.getElementsByClassName('sum-check');
+if (nameYour)
 
 for (var y=0; y <priceCheck.length; y++){
 
@@ -19,23 +23,30 @@ for (var y=0; y <priceCheck.length; y++){
      totalPrice.innerHTML =  '$' +  initialPrice;
 
      
-     var discountCode ='1A3B1H';
+     var discountCodePercentTwenty ='1A3B1H';
      var discount = 20;
      var discountCodePercentTen=['CODE1','CODE2', 'CODE3','CODE4', 'CODE5'];
      var discount = 10;
 
-     if (disCoupon.value === discountCode){
-         var priceDiscount = initialPrice - (initialPrice / 100 * 20);
-         totalPrice.innerHTML = '$' + priceDiscount.toFixed(2);
-         
-     } 
-   
+    var flag = false;
+
     for (var i = 0; i < discountCodePercentTen.length; i++){
-        if (discountCodePercentTen[i] ===disCoupon.value){
-        var priceDiscountTen = initialPrice - (initialPrice / 100 * 10);
-         totalPrice.innerHTML = '$' + priceDiscountTen.toFixed(2);
-        } else if (!discountCodePercentTen[i] === disCoupon.value){
-            alert('Invalid');
+        if (discountCodePercentTen[i] === disCoupon.value){
+            flag = true;
         }
     }
+
+    if(flag) {
+
+        var priceDiscountTen = initialPrice - (initialPrice / 100 * 10);
+        totalPrice.innerHTML = '$' + priceDiscountTen.toFixed(2);
+
+    } else if (disCoupon.value === discountCodePercentTwenty){
+        var priceDiscountTwenty = initialPrice - (initialPrice / 100 * 20);
+        totalPrice.innerHTML = '$' + priceDiscountTwenty.toFixed(2);
+    } 
+    else if(disCoupon.value.length > 0) {
+        alert ('Your discount code is invalid');
+    }
 })
+
