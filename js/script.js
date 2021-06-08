1af6b45636a845acbc70dadcc47c2e961097a9aa
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-06-08 11:31:48 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var createPrice= document.getElementsByClassName('create-prices-btn')[0];
 var totalPrice = document.getElementById('total-price');
 
-var discountCode=['CODE1','CODE2', 'CODE3','CODE4', 'CODE5'];
+
 var disCoupon = document.getElementById('dis-coupon');
 createPrice.addEventListener('click', function(){
 
@@ -18,4 +18,24 @@ for (var y=0; y <priceCheck.length; y++){
 }
      totalPrice.innerHTML =  '$' +  initialPrice;
 
+     
+     var discountCode ='1A3B1H';
+     var discount = 20;
+     var discountCodePercentTen=['CODE1','CODE2', 'CODE3','CODE4', 'CODE5'];
+     var discount = 10;
+
+     if (disCoupon.value === discountCode){
+         var priceDiscount = initialPrice - (initialPrice / 100 * 20);
+         totalPrice.innerHTML = '$' + priceDiscount.toFixed(2);
+         
+     } 
+   
+    for (var i = 0; i < discountCodePercentTen.length; i++){
+        if (discountCodePercentTen[i] ===disCoupon.value){
+        var priceDiscountTen = initialPrice - (initialPrice / 100 * 10);
+         totalPrice.innerHTML = '$' + priceDiscountTen.toFixed(2);
+        } else if (!discountCodePercentTen[i] === disCoupon.value){
+            alert('Invalid');
+        }
+    }
 })
